1abdb55d24adf4eb67620bb0498b11c6c49ed774
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-07-04 11:39:06 +0300

    added declaration and implementation for copy constructor and operator= overloading

--- Assignment_4/Text/Text.cpp
+++ Assignment_4/Text/Text.cpp
@@ -6,8 +6,20 @@ Text::Text() {
 }
 Text::Text(char *text) {
     this->text = new char[strlen(text)+1];
-    strcpy(this->text, text);
 }
+
+Text::Text(const Text &other) {
+    this->text = new char[strlen(other.text) + 1];
+}
+
+Text Text::operator=(char element) {
+    delete[] text;
+    text = new char[2];
+    text[0] = element;
+    text[1] = '\0';
+    return *this;
+}
+
 Text::~Text() {
     delete[] text;
 }
--- Assignment_4/Text/Text.h
+++ Assignment_4/Text/Text.h
@@ -6,6 +6,8 @@ private:
 public:
     Text();
     Text(char* text);
+    Text(const Text& other);
+    Text operator=(char element);
     ~Text();
     char* getText() const;
 };
